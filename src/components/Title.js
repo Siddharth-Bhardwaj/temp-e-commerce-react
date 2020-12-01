b6f0c3de7ac197a91c61a46ffbdc24104bc6ac47
Siddharth Bhardwaj <siddharth.bhardwaj2051999@gmail.com>
2020-12-01 10:55:08 +0530

    h1 to h2

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,10 +4,10 @@ export default function Title({ name, title }) {
     return (
         <div className='row'>
             <div className='col-10 mx-auto my-2 text-center text-title'>
-                <h1 className='text-capitalize font-weight-bold'>
+                <h2 className='text-capitalize font-weight-bold'>
                     {name} <br></br>{" "}
                     <strong className='text-blue'>{title}</strong>
-                </h1>
+                </h2>
             </div>
         </div>
     );
